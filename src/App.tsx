c29bf057428844114ca012c5ed9cb97113260616
hlista <hlista91@gmail.com>
2025-01-09 22:00:38 -0600

    adding backspace

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,11 @@ function App() {
   const [selectedLetters, setSelectedLetters] = useState<string[]>([])
 
   // used for coloring
+  guessedWords.forEach(guessedWord => {
+    guessedWord.split('').forEach((guessedLetter, index) => {
+      if (guessedLetter === wordToGuess[index]) console.log(`${guessedLetter} is correct`)
+    })
+  })
   const [correctLetters, setCorrectLetters] = useState<string[]>([])
 
   const [incorrectLetters, setIncorrectLetters] = useState<string[]>([])
@@ -29,21 +34,30 @@ function App() {
     setSelectedLetters(currentLetters => [...currentLetters, letter])
   }, [selectedLetters])
 
+  const popSelectedLetters = useCallback(() => {
+    if (selectedLetters.length === 0) return
+
+    setSelectedLetters(currentLetters => {
+      const newArray = [...currentLetters]
+      newArray.pop();
+      return newArray;
+    })
+  }, [selectedLetters])
+
   const evalSelectedLetters = useCallback(() => {
     const selectedWord = selectedLetters.join('')
     const wordToGuessArray = wordToGuess.split('')
     if (selectedLetters.length < 5) return
-    selectedLetters.forEach((letter, index) => {
-      if (letter === wordToGuessArray[index] && !correctLetters.includes(letter)) {
-        setCorrectLetters(currentLetters => [...currentLetters, letter])
-      }
-      if (wordToGuessArray.includes(letter) && !correctLetters.includes(letter) && !hintLetters.includes(letter)) {
-        setHintLetters(currentLetters => [...currentLetters, letter])
-      }
-      if (!wordToGuessArray.includes(letter)) {
-        setIncorrectLetters(currentLetters => [...currentLetters, letter])
-      }
-    })
+    // selectedLetters.forEach((letter, index) => {
+    //   if (letter === wordToGuessArray[index] && !correctLetters.includes(letter)) {
+    //     setCorrectLetters(currentLetters => [...currentLetters, letter])
+    //   } else if (wordToGuessArray.includes(letter) && !correctLetters.includes(letter) && !hintLetters.includes(letter)) {
+    //     setHintLetters(currentLetters => [...currentLetters, letter])
+    //   } else if (!wordToGuessArray.includes(letter) && !incorrectLetters.includes(letter)) {
+    //     setIncorrectLetters(currentLetters => [...currentLetters, letter])
+    //   }
+    // })
+
     if (wordToGuess === selectedWord) {
       setIsWinner(true)
     } else {
@@ -56,7 +70,7 @@ function App() {
     const handler = (e: KeyboardEvent) => {
       const key = e.key
       if (!key.match(/^[a-z]$/)) return
-      console.log(selectedLetters)
+
       e.preventDefault()
       addSelectedLetters(key)
     }
@@ -72,7 +86,6 @@ function App() {
     const handler = (e: KeyboardEvent) => {
       const key = e.key
       if (key !== "Enter") return
-      console.log("here")
       e.preventDefault()
       evalSelectedLetters()
       setSelectedLetters([])
@@ -82,6 +95,21 @@ function App() {
 
     return () => {
       document.removeEventListener("keypress", handler)
+    }
+  }, [selectedLetters])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+      if (key !== "Backspace") return
+      e.preventDefault()
+      popSelectedLetters()
+    }
+
+    document.addEventListener("keydown", handler)
+
+    return () => {
+      document.removeEventListener("keydown", handler)
     }
   }, [selectedLetters])
 
